49a8a79432b9c9d77742a1decfeaecc37ac36836
Arthur Carvalho <arthur_07q@hotmail.com>
2024-11-07 16:35:21 -0300

    Include a memory leak example detectable by infer

---
+++ infer/memory_leak.c
@@ -0,0 +1,26 @@
+// memory_leak.c
+// error: NULLPTR_DEREFERENCE
+// infer run -- gcc -c memory_leak.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void example_function()
+{
+    int *arr = (int *)malloc(10 * sizeof(int));
+
+    // Some operations with arr
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = i * i;
+    }
+
+    // Missing free(arr); - Memory leak here
+    printf("Example function executed\n");
+}
+
+int main()
+{
+    example_function();
+    return 0;
+}
